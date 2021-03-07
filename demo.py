8d5621ed97f525e9ab9d41322fc9678f7d54948f
Natalie Volk <natalievolk@rogers.com>
2021-03-06 19:03:33 -0500

    basic flask app implementation

--- demo.py
+++ demo.py
@@ -2,19 +2,23 @@ import os, io
 from google.cloud import vision
 from google.cloud.vision_v1 import types
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'My First Project-194ff9610555.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'pleasedontmine.json'
 
-client = vision.ImageAnnotatorClient()
+def get_handwritten(filename):
+    client = vision.ImageAnnotatorClient()
 
-FOLDER_PATH = os.getcwd()+ '\\Images'
-IMAGE_FILE = 'handwriting1.jpeg'
-FILE_PATH = os.path.join(FOLDER_PATH, IMAGE_FILE)
+    FOLDER_PATH = os.getcwd()+ '/Images'
+    IMAGE_FILE = filename
+    FILE_PATH = os.path.join(FOLDER_PATH, IMAGE_FILE)
 
-with io.open(FILE_PATH, 'rb') as image_file:
-    content = image_file.read()
+    with io.open(FILE_PATH, 'rb') as image_file:
+        content = image_file.read()
 
-image = vision.Image(content=content)
-response = client.document_text_detection(image=image)
-docText = response.full_text_annotation.text
-print(docText)
+    image = vision.Image(content=content)
+    response = client.document_text_detection(image=image)
+    docText = response.full_text_annotation.text
+    #print(docText)
+    return docText
 
+if __name__ == "__main__":
+    get_handwritten()
