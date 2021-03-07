ebd9d335d24f93d98c90f3577de8258bf4374736
Jason Cai <jjcai868@gmail.com>
2021-03-06 20:53:53 -0500

    weighted

--- demo.py
+++ demo.py
@@ -7,7 +7,7 @@ from textblob import Word
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'pleasedontmine.json'
 
 def get_handwritten(filename):
-    client = vision.ImageAnnotatorClient()
+    client = vision.ImageAnnotatorClient()  
 
     FOLDER_PATH = os.getcwd()+ '/Images'
     IMAGE_FILE = filename
@@ -19,15 +19,18 @@ def get_handwritten(filename):
     image = vision.Image(content=content)
     response = client.document_text_detection(image=image)
     docText = response.full_text_annotation.text
+    
+    print(docText)
 
     text = docText.lower().replace('.', '').replace('(', '').replace(')', '').replace(',', '').replace('--', ' ').replace('-', ' ').replace(':', '').replace(';', '').replace('? ', '.').replace('!', '.').replace('\n', ' ').split()
     print(text)
 
     spell = SpellChecker()
     corrections = {}
+
+    print(spell.unknown(text))
     for flagged_word in spell.unknown(text):
         word = Word(flagged_word)
-
         choices = []
         for choice in word.spellcheck():
             if choice[1] > 0.05 and len(choices) <= 4:
@@ -35,7 +38,7 @@ def get_handwritten(filename):
 
         if len(choices) > 0:
             corrections[flagged_word] = choices
-
+        
     return corrections
     
 if __name__ == "__main__":
